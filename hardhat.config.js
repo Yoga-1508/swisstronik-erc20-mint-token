b92ec6f1bc23d8c4cc03b792cbaedc743feff200
Yoga-1508 <56836655+Yoga-1508@users.noreply.github.com>
2024-07-25 11:18:54 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0x0de5837fcfd6feb665ae1965504223cfff715816c21099a563b9035cd1a4a950"], //Your private key starting with "0x" 
+      accounts: [""], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
